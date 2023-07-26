7e178a0b5724c04992b009a5c4859f7a8e11b5d3
tajmuhammed123 <muhammedtaj7@gmail.com>
2023-07-26 09:30:36 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef,useEffect,useState } from 'react';
 
 function App() {
+  
+  const [name,setName]=useState('')
+
+  const submitForm=(e)=>{
+    e.preventDefault()
+    console.log(name);
+    setName("")
+  }
+  const inputRef=useRef(null)
+
+  useEffect(()=>{
+    inputRef.current.focus()
+  })
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitForm}>
+        <input onChange={(e)=>{
+          if(e.target.value.trim() === ""){
+            return;
+          }
+          setName(e.target.value)}} value={name} ref={inputRef} type='text'/> <br />
+        <button >Submit</button>
+      </form>
     </div>
   );
 }
